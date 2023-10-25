272abf249b91bc1e686ae7b42de9157a0b4d9724
repo-copier <info@gruntwork.io>
2023-10-24 18:56:11 -0600

    Resolve references

--- test/terraform_gcp_example_test.go
+++ test/terraform_gcp_example_test.go
@@ -6,11 +6,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gruntwork-io/terratest/modules/gcp"
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/retry"
-	"github.com/gruntwork-io/terratest/modules/ssh"
-	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/terraform-modules-krish/terratest/modules/gcp"
+	"github.com/terraform-modules-krish/terratest/modules/random"
+	"github.com/terraform-modules-krish/terratest/modules/retry"
+	"github.com/terraform-modules-krish/terratest/modules/ssh"
+	"github.com/terraform-modules-krish/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
