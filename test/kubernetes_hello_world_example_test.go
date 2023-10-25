11eb01c021ac1675d8a371ff6d4dbd075519ba79
repo-copier <info@gruntwork.io>
2023-10-24 18:57:31 -0600

    Resolve references

--- test/kubernetes_hello_world_example_test.go
+++ test/kubernetes_hello_world_example_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 	"time"
 
-	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
-	"github.com/gruntwork-io/terratest/modules/k8s"
+	http_helper "github.com/terraform-modules-krish/terratest/modules/http-helper"
+	"github.com/terraform-modules-krish/terratest/modules/k8s"
 )
 
 func TestKubernetesHelloWorldExample(t *testing.T) {
