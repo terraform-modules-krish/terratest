3ce75f1f5c8a5fd2db318043283ebf2bdb4f8a94
repo-copier <info@gruntwork.io>
2023-10-24 18:56:44 -0600

    Resolve references

--- modules/aws/vpc.go
+++ modules/aws/vpc.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/terraform-modules-krish/terratest/modules/random"
 )
 
 // Vpc is an Amazon Virtual Private Cloud.
