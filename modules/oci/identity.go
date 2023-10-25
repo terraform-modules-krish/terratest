5ce7742fa73ec05d62a2e3f20993abeb3c917e24
repo-copier <info@gruntwork.io>
2023-10-24 18:56:41 -0600

    Resolve references

--- modules/oci/identity.go
+++ modules/oci/identity.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/logger"
-	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/terraform-modules-krish/terratest/modules/logger"
+	"github.com/terraform-modules-krish/terratest/modules/random"
 	"github.com/oracle/oci-go-sdk/common"
 	"github.com/oracle/oci-go-sdk/identity"
 )
