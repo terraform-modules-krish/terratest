9549a7d39f865821e2192b3c26a3020dbfe18f60
repo-copier <info@gruntwork.io>
2023-10-24 18:56:32 -0600

    Resolve references

--- modules/aws/ec2-syslog.go
+++ modules/aws/ec2-syslog.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/gruntwork-io/terratest/modules/logger"
-	"github.com/gruntwork-io/terratest/modules/retry"
+	"github.com/terraform-modules-krish/terratest/modules/logger"
+	"github.com/terraform-modules-krish/terratest/modules/retry"
 )
 
 // (Deprecated) See the FetchContentsOfFileFromInstance method for a more powerful solution.
--- modules/helm/install.go
+++ modules/helm/install.go
@@ -4,10 +4,10 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gruntwork-io/gruntwork-cli/errors"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/errors"
 	"github.com/stretchr/testify/require"
 
-	"github.com/gruntwork-io/terratest/modules/files"
+	"github.com/terraform-modules-krish/terratest/modules/files"
 )
 
 // Install will install the selected helm chart with the provided options under the given release name. This will fail
