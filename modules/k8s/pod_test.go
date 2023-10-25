30274b67ea0923e999770b0bcdca13eac076111c
repo-copier <info@gruntwork.io>
2023-10-24 18:57:00 -0600

    Resolve references

--- modules/k8s/pod_test.go
+++ modules/k8s/pod_test.go
@@ -17,7 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/terraform-modules-krish/terratest/modules/random"
 )
 
 func TestListPodsReturnsPodsInNamespace(t *testing.T) {
